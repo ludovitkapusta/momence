4440210232896400ba57118706f0d699c97a4e73
Ludovit Kapusta <ludovit.kapusta@gmail.com>
2023-01-13 12:13:38 +0100

    added main App.tsx

---
+++ src/components/App.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+type AppProps = {}
+
+export const App = ({}: AppProps) => <h1>Momence technical task</h1>
--- src/index.tsx
+++ src/index.tsx
@@ -6,4 +6,4 @@ import { App } from './components/App'
 const container = document.getElementById('root')
 const root = createRoot(container)
 
-root.render(<App num={1234} />)
+root.render(<App />)
