187c4f760944e53d45111faf4ccc0faf178df612
Ludovit Kapusta <ludovit.kapusta@gmail.com>
2023-01-14 18:05:09 +0100

    add styled-components

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,11 @@
 import React from 'react'
+import { Reset } from 'styled-reset'
 
 type AppProps = {}
 
-export const App = ({}: AppProps) => <h1>Momence technical task</h1>
+export const App = ({}: AppProps) => (
+  <>
+    <Reset />
+    <h1>Momence technical task</h1>
+  </>
+)
